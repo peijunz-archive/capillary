0b2926b6c1196c099f5deea063e39b7c56f07ef1
Peijun Zhu <zpj.ustc@gmail.com>
2017-06-29 23:57:22 -0400

    save

--- draw.py
+++ draw.py
@@ -1,12 +1,33 @@
 from capillary import edge, fitting, analyse, display
 from pylab import *
-rc('text', usetex=True)
-df0=[analyse.analyse_raw(i) for i in range(0, 8)]
-# Three subplots sharing both x/y axes
+from matplotlib.ticker import AutoMinorLocator
+import pickle
 
-def draw(kind='mirror', start=10):
-    cla()
+rc('text', usetex=True)
+start=30
+ptsize=2
+lw=0.3
+try:
+    pickle.load(open('30seconds.pkl', 'r'))
+except:
+    df0=[analyse.analyse_raw(i) for i in range(0, 8)]
     dfs=[i.loc[i['time']>-start] for i in df0]
+    #pickle.dump(dfs, open('30seconds.pkl', 'w'))
+
+brk=-1
+# Three subplots sharing both x/y axes
+solid=0.7
+hallow=0.7
+lab=('',
+     r'$T/L=1/25\mathrm{, trial\ 1}$',
+     r'$T/L=1/25\mathrm{, trial\ 2}$',
+     r'$T/L=1/25\mathrm{, trial\ 1}$',
+     r'$T/L=1/25\mathrm{, trial\ 2}$',
+     r'$T/L=1/25\mathrm{, trial\ 3}$',
+     r'$T/L=1/50$',
+     r'$T/L=1/50$')
+def draw(kind='mirror', ):
+    cla()
     nrow = 2
     f, ax = plt.subplots(nrow, 2, sharex=True)
     #ax[0, 0].axvline(0, linestyle='-', color='#cccccc', linewidth=0.8)
@@ -30,55 +51,78 @@ def draw(kind='mirror', start=10):
     cols = (r'$\textrm{tip-tip}$', r'$\textrm{tip-side}$')
     for i, col in enumerate(cols):
         ax[0, i].set_title(col, fontsize=10)
-    rows = (r'$r$', r'$\theta_1, \theta_2$', r'$\theta_1, \theta_2$')[:nrow]
+    rows = (r'$r/\mathrm{px}$', r'$\theta_1, \theta_2/\mathrm{rad}$', r'$\theta_1, \theta_2$')[:nrow]
     for i, row in enumerate(rows):
-        ax[i, 0].set_ylabel(row, fontsize=10, rotation=0)
+        ax[i, 0].set_ylabel(row, fontsize=10)#, rotation=0)
+    ax[1, 0].set_xlabel(r'$t/\mathrm{s}$', fontsize=10, rotation=0)
+    ax[1, 1].set_xlabel(r'$t/\mathrm{s}$', fontsize=10, rotation=0)
 
-
-    tt=[(3, 'o'), (4, 's'), (5, '>'), (7, '^')]
-    ts=[(1, 'o'), (2, 's'), (6, '^')]
+    tt=[(3, 'oC0'), (4, 'sC1'), (5, '>C2'), (7, '*C3')]
+    ts=[(1, 'oC0'), (2, 'sC1'), (6, '>C2')]
 
     l=[(*i, 0) for i in tt] + [(*i, 1) for i in ts]
 
-    iax = [f.add_axes([.14, .62, .15, .15]), 
-        f.add_axes([.54, .64, .15, .13])]
+    iax = [f.add_axes([.382, .75, .13, .13]), 
+        #f.add_axes([.545, .643, .15, .13]), 
+        f.add_axes([.77, .75, .13, .13])]
     alp=1
     for i, m, typ in l:
         df = dfs[i]
         dfp = df.loc[df['time']<0]
-        ax[0, typ].scatter(dfp['time'], dfp['rho'], s=1, marker=m, label=r"${}$".format(i), alpha=alp, lw = 0)
-        iax[typ].scatter(log(-dfp['time']), log(dfp['rho']), s=1, marker=m, alpha=alp, lw = 0)
-        ax[1, typ].scatter(df['time'], df['theta1'], s=1, marker=m, label=r"$\theta_1, {}$".format(i), alpha=alp, lw = 0)
-        #ax[1, typ].plot(df['time'], (-1)**(1+0)*df['theta2'], m, markersize=1, label=r"${}\ \theta_2$".format(i))
-        if kind == 'mirror' or not typ:
-            lab2=r"$\theta_2, {}$".format(i)
-            sign=1
-        else:
-            lab2=r"$\vartheta_2, {}$".format(i)
-            sign=-1
-        ax[1, typ].scatter(df['time'], -sign*df['theta2'], s=1, marker=m, label=lab2, alpha=alp, lw = 0)
-        if nrow > 2:
-            dfn = df.loc[df['time']>-1]
-            ax[2, typ].plot(dfn['time'], -sign*dfn['theta2'], m, markersize=1)
-            ax[2, typ].plot(dfn['time'], dfn['theta1'], m, markersize=1)
-            ax[2, typ].set_yticks([-pi/3, -pi/6, 0, pi/6, pi/3])
-            ax[2, typ].set_yticklabels([r'$-\frac{\pi}{3}$', r'$-\frac{\pi}{6}$', r'$0$', r'$\frac{\pi}{6}$', r'$\frac{\pi}{3}$'])
 
-    ax[0, 0].legend(loc='upper right', prop={'size': 8}, handletextpad=-0.5, borderpad=0.3, labelspacing=0, markerscale=2.)
-    ax[0, 1].legend(loc='upper right', prop={'size': 8}, handletextpad=-0.5, borderpad=0.3, labelspacing=0, markerscale=2.)
-    ax[1, 0].legend(loc='lower center', prop={'size': 8}, handletextpad=-0.5, borderpad=0.3, labelspacing=0, markerscale=2.)
-    ax[1, 1].legend(loc='upper right', prop={'size': 8}, handletextpad=-0., borderpad=0.3, labelspacing=0, borderaxespad=0, markerscale=2.)
-    ax[0,0].set_xlim(left=-start-0.1, right=0.3*start)
-    for i in iax:
-        i.set_xticks(())
-        i.set_yticks(())
-    f.suptitle('Starting at time -{}, {} convention for tip-side events'.format(start, kind))
+        ldf = df.loc[df['time']<brk]
+        ldfp = dfp.loc[dfp['time']<brk]
+        rdf = df.loc[df['time']>=brk]
+        rdfp = dfp.loc[dfp['time']>=brk]
+
+        kargs={'markersize':ptsize,
+               'alpha':solid,
+               'lw': lw,
+               'markeredgewidth':0
+               }
+        ax[0, typ].plot(np.array(ldfp['time']), np.array(ldfp['rho']), m, **kargs)
+        ax[0, typ].plot(rdfp['time'], rdfp['rho'], m+'-', label='$r\ \ $'+lab[i], **kargs)
+
+        iax[typ].loglog(-ldfp['time'], ldfp['rho'], m, **kargs)
+        #iax[typ].plot(log(-ldfp['time']), log(ldfp['rho']), m, **kargs)
+        iax[typ].loglog(-rdfp['time'], rdfp['rho'], m+'-', **kargs)
+
+        ax[1, typ].plot(np.array(ldf['time']), np.array(ldf['theta1']), m, **kargs)
+        ax[1, typ].plot(rdf['time'], rdf['theta1'], m+'-', label=r"$\theta_1\ \ $"+lab[i], **kargs)
+        edge = 0.07*ptsize**2
+        ms=ptsize-edge*0.5
+        ax[1, typ].plot(np.array(ldf['time']), np.array(-ldf['theta2']), m, markersize=ptsize, lw =lw, markerfacecolor='none', markeredgewidth=edge, alpha=hallow)
+        ax[1, typ].plot(rdf['time'], -rdf['theta2'], m+'-', markersize=ms, label=r"$\theta_2\ \ $"+lab[i], lw = lw, markerfacecolor='none', markeredgewidth=edge, alpha=hallow)
+    x=np.array([0.0333, 30])
+    y=120*x**(1/8)
+    iax[0].loglog(x, y, 'k:', lw=0.4)
+    iax[1].loglog(x, y, 'k:', lw=0.4)
+    ax[0, 0].legend(loc='center left', prop={'size': 7}, borderpad=0.3, labelspacing=0, markerscale=2.)
+    ax[0, 1].legend(loc='center left', prop={'size': 7}, borderpad=0.3, labelspacing=0, markerscale=2.)
+    ax[1, 0].legend(loc='lower left', prop={'size': 7}, borderpad=0.3, labelspacing=0, markerscale=2.)
+    ax[1, 1].legend(loc='upper left', prop={'size': 7}, borderpad=0.3, labelspacing=0, markerscale=2.)
+    ax[0,0].set_xlim(left=-start-0.1)
+    ax[0,0].set_ylim(top=400)
+    ax[1,0].plot(-3, 0, marker=(3, 0, 60), color='C3', markersize=15, linestyle='None', markeredgewidth=0)
+    ax[1,0].plot(-2.78, -0.29, marker=(3, 0, -110), color='C3', markersize=15, linestyle='None', markeredgewidth=0)
+
+    ax[1,0].plot(-3, -0.9, marker=(3, 0, 0), color='C3', markersize=15, linestyle='None', markeredgewidth=0)
+    ax[1,0].plot(-3, -1.05, marker=(3, 0, -180), color='C3', markersize=15, linestyle='None', markeredgewidth=0)
+
+    #iax[0].minorticks_off()
+    iax[1].tick_params(labelsize=6, pad=0.3)
+    iax[0].tick_params(labelsize=6, pad=0.3)
+    plt.setp(iax[0].get_yminorticklabels(), visible=False)
+    plt.setp(iax[1].get_yminorticklabels(), visible=False)
+    #for i in iax:
+        #i.set_xticks(())
+        #i.set_yticks(())
+    #f.suptitle('Starting at time -{}, {} convention for tip-side events'.format(start, kind))
     f.subplots_adjust(hspace=0, wspace=0);
-    name='{}-{}.pdf'.format(start, kind)
-    savefig('combined/'+name, bbox_inches='tight')
+    name='{}-{}'.format(start, kind)
+    savefig('combined/'+name+'.pdf', bbox_inches='tight')
+    savefig('combined/'+name+'.svg', bbox_inches='tight')
     print(name, 'saved')
 
 if __name__ == "__main__":
-    for i in [10, 20, 30, 50]:
-        draw('mirror', i)
-        draw('central', i)
+    draw()
